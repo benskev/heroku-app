a37b3a672de78be1ca5360ae5725f62f116fce10
benskev <ben@benskev.co.za>
2016-08-03 01:02:09 +0200

    Recommitting

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
  {
 	 $ip = $_SERVER['REMTOE_ADDR'];
  }
- echo "IP: " . $ip;
- print_r($_SERVER);
+ echo $ip;
+ $f = fopen("ips.txt","a") or die("Unable to open appended file!");
+ fwrite($f, date("Y-n-j H:i:s") . " - " . $ip) or die("Cannot write to file");
+ fclose($f) or die("Cannot close the file");
+ //print_r($_SERVER);
 ?>
