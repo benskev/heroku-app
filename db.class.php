7130097a35b902c22a4db94ca8a21ba13c8781b0
benskev <ben@benskev.co.za>
2016-08-04 03:21:09 +0200

    added DB components

---
+++ db.class.php
@@ -0,0 +1,20 @@
+class MyDB extends SQLite3
+  {
+     function __construct()
+     {
+        $this->open('test.db');
+     }
+
+     function run($sql)
+     {
+           $ret = $this->exec($sql);
+           if(!$ret){
+              echo $this->lastErrorMsg();
+           } else {
+             $row = $ret->fetchArray(SQLITE3_ASSOC)
+             //echo "Records created successfully\n";
+           }
+           return $row;
+           $db->close();
+     }
+  }
--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
-/* 
+/*
  * show me your ip!
  */
+include "db.class.ophp";
+
  ini_set('display_errors','on');
  header("Content-Type: text/plain");
 
@@ -18,5 +20,7 @@
  $f = fopen("ips.txt","a") or die("Unable to open appended file!");
  fwrite($f, date("Y-n-j,H:i:s") . "," . $ip . "\n") or die("Cannot write to file");
  fclose($f) or die("Cannot close the file");
+ $db = new MyDB();
+ $db->run("insert into ip (address,aDate,aTime) values ('" . $ip . "','".date("Y-n-j")."','".date("H:i:s")."');") or die ("ERROR");
  //print_r($_SERVER);
 ?>
