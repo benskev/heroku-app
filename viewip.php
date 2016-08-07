c6c958cc512efd9eea3f80957b14c014e1d8d00e
ben <benskev@users.noreply.github.com>
2016-08-07 15:02:22 +0200

    Update viewip.php

--- viewip.php
+++ viewip.php
@@ -45,6 +45,7 @@ if (file_exists("test.db"))
 $db = new MyDB();	// process the line read.
 $r=$db->get("select * from ip");
 print_r($r);
+die();
 foreach ($r as $i->$res)
 {
 	echo "<td>".$res['aDate']."</td><td>".$res['aTime']."</td><td>".$ip['address']."</td></tr>\n";
